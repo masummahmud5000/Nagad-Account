e218e00d31a6dcda3b82883f8c867145ea0dc33f
masummahmud5000 <mdmasumpresent@gmail.com>
2026-04-04 00:06:51 +0600

    Change balance formatting from 'en-IN' to 'en-US'

--- app/_clientComponents/balance.tsx
+++ app/_clientComponents/balance.tsx
@@ -18,7 +18,7 @@ export default function Balance(){
         let tk = localStorage?.getItem('balance')
         if (tk){
             let result = Number(tk)
-            let final = result.toLocaleString('en-IN')
+            let final = result.toLocaleString('en-US')
             return setBalance(final.toString())
         }else{
             localStorage.setItem('balance','0')
